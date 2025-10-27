6bb8e8f698437e37fe83c08a40b961ce0d76fa97
Wannes Gennar <wannes.gennar+github@gmail.com>
2025-10-27 16:31:51 +0100

    chore: add Redis server

--- src/Homemade.AppHost/AppHost.cs
+++ src/Homemade.AppHost/AppHost.cs
@@ -5,6 +5,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.AddRabbitMQ("rabbitmq")
     .WithManagementPlugin();
 
+var redis = builder.AddRedis("redis");
+redis.WithDbGate(configure => configure.WithParentRelationship(redis));
+
 var database = builder.AddPostgres("database");
 var recipesDb = database.AddDatabase("recipes");
 var usersDb = database.AddDatabase("users");
