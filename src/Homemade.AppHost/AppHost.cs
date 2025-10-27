33e7bf276a26cfcd147343cc108924bd385e9e37
Wannes Gennar <wannes.gennar+github@gmail.com>
2025-10-27 16:26:01 +0100

    chore: add RabbitMQ

--- src/Homemade.AppHost/AppHost.cs
+++ src/Homemade.AppHost/AppHost.cs
@@ -2,6 +2,9 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddRabbitMQ("rabbitmq")
+    .WithManagementPlugin();
+
 var database = builder.AddPostgres("database");
 var recipesDb = database.AddDatabase("recipes");
 var usersDb = database.AddDatabase("users");
